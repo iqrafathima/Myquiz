a287aafa7baefad4ddb61bb2087688655eb07f93
iqrafathima <74598692+iqrafathima@users.noreply.github.com>
2021-06-12 12:26:56 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,72 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+    //write code to change the background color here
+    background("cyan");
+    fill(0);
+    //write code to show a heading for showing the result of Quiz
+    textSize(28);
+    text("Result of the quiz",340,50);
+    text("----------------------------",320, 65);
+
+    //call getContestantInfo( ) here
+    Contestant.getPlayerInfo();
+    if(allContestants !== undefined){
+      debugger;
+      var correctAns = "2";
+      var display_Answers = 230;
+      fill("Blue");
+      textSize(16);
+      text("*Note: Contestant who answered correct are highlighted in green color!",130,230);
+
+    //write condition to check if contestantInfor is not undefined
+    for(var plr in allContestants){
+      debugger;
+      var correctAns = "2";
+      if (correctAns === allContestants[plr].answer)
+        fill("green");
+      else
+        fill("red");
+
+    //write code to add a note here
+  
+
+    //write code to highlight contest who answered correctly
+    display_Answers+=30;
+    textSize(20);
+    text(allContestants[plr].name + ": " + allContestants[plr].answer, 250,display_Answers)
+    
+  }
+}
+}
+}
